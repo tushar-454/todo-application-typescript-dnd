3078b22afb3d9ab1d0b25f07d2e5789f96c701b5
tushar-454 <imtushar454@gmail.com>
2024-01-20 23:30:51 +0600

    simple todo application to practice typescript and implement react dnd

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import HeadTitle from './Components/HeadTitle';
+
+const App = () => {
+  return (
+    <main>
+      <HeadTitle />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/Components/HeadTitle.tsx
@@ -0,0 +1,11 @@
+const HeadTitle = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>
+        Todo Application with TypeScript
+      </h1>
+    </div>
+  );
+};
+
+export default HeadTitle;
