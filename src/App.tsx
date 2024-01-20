08fcfe36ba97b85ec243eb6246d9c66803aaf8ac
tushar-454 <imtushar454@gmail.com>
2024-01-20 23:38:07 +0600

    add projec title with short description

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import HeadTitle from './Components/HeadTitle';
 
 const App = () => {
   return (
-    <main>
+    <main className='bg-sky-300 h-screen w-screen'>
       <HeadTitle />
     </main>
   );
--- src/Components/HeadTitle.tsx
+++ src/Components/HeadTitle.tsx
@@ -1,9 +1,15 @@
 const HeadTitle = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>
+      <h1 className='text-2xl font-bold text-center py-3'>
         Todo Application with TypeScript
       </h1>
+      <p className='text-gray-800 font-medium text-center w-1/4 mx-auto'>
+        A raw todo application using TypeScript for better developer experience
+        and practice TypeScript as well. And another important part is implement
+        React beautifull dnd package to handle todo to completed using drag and
+        drop
+      </p>
     </div>
   );
 };
