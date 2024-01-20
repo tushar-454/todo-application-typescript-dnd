54d374b14e84331631cfd9396bd5fd9d828d3582
tushar-454 <imtushar454@gmail.com>
2024-01-20 23:41:05 +0600

    configure tailwind class getting right order, setup with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import HeadTitle from './Components/HeadTitle';
 
 const App = () => {
   return (
-    <main className='bg-sky-300 h-screen w-screen'>
+    <main className='h-screen w-screen bg-sky-300'>
       <HeadTitle />
     </main>
   );
--- src/Components/HeadTitle.tsx
+++ src/Components/HeadTitle.tsx
@@ -1,10 +1,10 @@
 const HeadTitle = () => {
   return (
     <div>
-      <h1 className='text-2xl font-bold text-center py-3'>
+      <h1 className='py-3 text-center text-2xl font-bold'>
         Todo Application with TypeScript
       </h1>
-      <p className='text-gray-800 font-medium text-center w-1/4 mx-auto'>
+      <p className='mx-auto w-1/4 text-center font-medium text-gray-800'>
         A raw todo application using TypeScript for better developer experience
         and practice TypeScript as well. And another important part is implement
         React beautifull dnd package to handle todo to completed using drag and
